f8c0c851eab86eafe2e4851c9c2e0dd65b4719b9
ciarancooke9 <ciaran.cooke9@mail.dcu.ie>
2021-12-10 07:27:33 -0800

    Postgres database added

--- it_arch/settings.py
+++ it_arch/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +77,13 @@ WSGI_APPLICATION = 'it_arch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django',
+        'USER': 'django@it-arch-db',
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': 'it-arch-db.postgres.database.azure.com',
+        'PORT': '5432',
+        'OPTIONS': {"sslmode": "require"},
     }
 }
 
