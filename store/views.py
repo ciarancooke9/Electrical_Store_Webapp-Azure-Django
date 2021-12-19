9e5e8caba6a141267789a5049c1802b45f366bf4
ciarancooke9 <ciaran.cooke9@mail.dcu.ie>
2021-12-19 05:57:48 -0800

    dynamic index

--- store/views.py
+++ store/views.py
@@ -4,8 +4,13 @@ from . models import Category, Product
 
 # Create your views here.
 def index(request):
+    phones = Product.objects.filter(category__name='Phones')
+    laptops = Product.objects.filter(category__name='Laptops')
+    essentials = Product.objects.filter(category__name='Household Essentials')
     context = {
-
+        'phones': phones,
+        'laptops': laptops,
+        'essentials': essentials,
     }
     return render(request,"store/index.html",context)
 
